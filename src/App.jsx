aa1618e7cd6c86f7a07c3b300684f422b699e260
Qasim Fida <qasimfida0054@gmail.com>
2025-06-27 21:05:14 +0500

    copy changes and style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,29 @@ function App() {
   return (
     <>
    
-    <div className="min-h-screen w-screen bg-black flex items-center justify-center font-helvetica-neue">
-      <div className="w-full min-h-screen flex flex-col items-center justify-center text-center relative">
-        <div className="content">
+    <div className="flex items-center justify-center w-screen min-h-screen px-5 bg-black font-helvetica-neue">
+      <div className="relative flex flex-col items-center justify-center w-full min-h-screen text-center">
+        <div className="flex items-center content">
+        <h2 className="font-bold deep-text">DeepFi</h2>
         <img 
           src={GamesAnimation} 
           alt="Games Animation" 
-          className="w-[500px] " 
+          className="w-[420px] mt-2" 
         />
         </div>
         
-        <h2 className="text-[#D0D0D0] text-[45px] font-[400] mb-[61px] mt-[0px] tracking-wide">COMING JULY 2025</h2>
-        <p className="text-white text-[32px] font-normal mb-[60px] mt-[0px] leading-[30px] max-w-[860px]">
-          The first Solana-based site featuring original games that
-          reward you in $SOL, even if you lose.
-        </p>
+        <h2 className="text-white md:text-[45px] text-3xl font-normal mb-8  tracking-wide">COMING JULY 2025</h2>
+        <p className="text-white md:text-2xl text-xl font-normal md:mb-19 mb-10 mt-0 max-w-[640px]">
+          The first Solana-based site featuring original games that reward you in $SOL, even if you lose.</p>
         <a href="https://x.com/deepfinews" target="_blank" rel="noopener noreferrer">
-          <button className="border-[2px] border-white rounded-[24px] px-[24px] py-[21px] text-white text-[18px] font-bold mb-[60px] mt-0 hover:bg-white hover:text-black transition-colors duration-200 flex items-center gap-[12px]">
-            <FaXTwitter className="text-[20px]" /> FOLLOW US
+          <button className="border-2 border-white rounded-[24px] px-6 h-14 text-white text-lg font-bold mb-7 mt-0 hover:bg-white hover:text-black transition-colors duration-200 flex items-center gap-[12px]">
+            <FaXTwitter className="text-xl" /> FOLLOW US
           </button>
         </a>
-        <p className="text-white text-[24px] mb-20 mt-[0px]">Powered through the magic of <span className="font-[700]">$DRIP</span></p>
-        <footer className="text-white text-[16px] fixed bottom-[0px] flex items-center justify-center gap-[12px] font-[700] py-[16px] left-0 right-0 w-full text-center tracking-wide">
-          <p>©2025 DeepDex.</p>
-          <p>a DeepFi Project.</p>
-          <p>©All Rights Reserved.</p>   
+        <p className="mt-0 mb-20 text-base text-white">Powered through the magic of <b>$DRIP</b></p>
+        <footer className="fixed bottom-0 left-0 right-0 flex flex-wrap items-center justify-center w-full gap-3 py-4 text-base font-bold tracking-wide text-center text-white md:flex-nowrap md:flex">
+          <p>©2025 DeepFi Games, a DeepFi Project.</p>
+          <p>All Rights Reserved.</p>   
         </footer>
       </div>
     </div>
